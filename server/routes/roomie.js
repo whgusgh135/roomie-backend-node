aa7e9aa9ead32876ea85cf2c0420d59c79d1869a
Hyunho Cho <whgusgh1357@gmail.com>
2019-01-07 21:43:28 +1300

    add more roomie api

--- server/routes/roomie.js
+++ server/routes/roomie.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Roomie = require("../controllers/roomie");
 
-router.get("/:id", Roomie.findRoomie);
+router.get("/", Roomie.getRoomies)
+router.get("/:id", Roomie.selectRoomie);
 router.post("/", Roomie.createRoomie);
+router.put("/:id", Roomie.updateRoomie);
+router.delete("/:id", Roomie.deleteRoomie);
 
 module.exports = router;
